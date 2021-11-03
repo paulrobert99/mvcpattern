0344abd231541b2083f21aa927778216345c1cc3
paulrobert99 <poool990224@gmai.com>
2021-11-03 10:41:22 +0100

    added to a public repo

---
+++ Gyakorlas/Controllers/CalcController.cs
@@ -0,0 +1,26 @@
+﻿using Gyakorlas.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gyakorlas.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Sum(CalcModel model)
+        {
+            int c = model.A + model.B;
+            String output = c.ToString();
+
+            return Content(output);
+        }
+    }
+}
---
+++ Gyakorlas/Controllers/WelcomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gyakorlas.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Show()
+        {
+            String text = this.Request.Form["str"].ToString();
+
+            return Content(text);
+        }
+    }
+}
